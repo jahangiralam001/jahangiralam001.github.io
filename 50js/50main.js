efdf7326da7bf5bd63d91c68e9f75d8eb6656252
jahangiralam001 <75940006+jahangiralam001@users.noreply.github.com>
2020-12-14 17:32:10 +0600

    Update 50main.js

--- 50js/50main.js
+++ 50js/50main.js
@@ -10,17 +10,17 @@ function payment(){
   var Trimesterfee = document.getElementById('Trimesterfee').value;
   
  
-
  var totalfee = parseFloat(credits * Creditsfee);
- var scholarship =  parseFloat((totalfee * 50)/100);
- 
- var result =  parseFloat((scholarship * 25)/100);
 
- var results = parseFloat(result);
+ var results = parseFloat(totalfee);
 
  var resultss = parseFloat(Trimesterfee);
 
  var finalfee = parseFloat(resultss + results);
+
+ var scholarship =  parseFloat((finalfee * 50)/100);
+ 
+ var result =  parseFloat((scholarship * 25)/100);
 
 
 
@@ -30,10 +30,11 @@ function payment(){
  
 //Show the results
 document.getElementById('totalfee').innerHTML = "Your tuition fee is "+ totalfee +" Taka. " ;
+document.getElementById('finalfee').innerHTML = "Your total payable fee is "+ finalfee +" Taka. " ;
 
 document.getElementById('scholarshipfee').innerHTML = " After 50% Scholarship now your tuition fee is "+ scholarship +" Taka. " ;
 
-document.getElementById('firstpayment').innerHTML = "You need to pay " + finalfee + " Taka before 18November,2020." ;
+document.getElementById('firstpayment').innerHTML = "You need to pay " + result + " Taka before 18November,2020." ;
 
 
 document.getElementById('secondpayment').innerHTML = "You need to pay " + result + " Taka before 15December,2020.";
